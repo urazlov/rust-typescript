2a7b3aeab6ecadc32a1878abbc457ead881109ed
Viktor <viktor@MacBook-Air-Viktor.local>
2025-04-13 17:09:40 +0400

    create iterator

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const list = [1, 2, 3].map(i => i += 1);;
+
+console.log(list);
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    let foo: Vec<_> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
+
+    println!("{:?}", foo);
 }
