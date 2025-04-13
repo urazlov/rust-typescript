d217622354fc29b0e1e665354403db84a03b692c
Viktor <urazlov.v@gmail.com>
2025-04-13 18:10:49 +0400

    iterator memory usage

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,9 @@ import * as fs from "fs";
 fs.readFile("./lines.txt", "utf8", (err, data) => {
   if (err) return;
 
-  data.split("\n").filter((_, i) => i % 2 === 0).forEach((line) => console.log(line));
+  data
+    .split("\n")
+    .filter((_, i) => i % 2 === 0)
+    .filter((_, i) => i > 1 && i < 4)
+    .forEach((line) => console.log(line));
 });
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,7 @@ fn main() {
     file.lines()
         .enumerate()
         .filter(|(idx, _)| idx % 2 == 0)
+        .skip(2)
+        .take(2)
         .for_each(|(_, line)| println!("{}", line));
 }
