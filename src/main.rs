c2e841227c8aa7becc557784c9e2980fb7589a34
Viktor <urazlov.v@gmail.com>
2025-04-13 17:43:47 +0400

    collect manually

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    let foo: Vec<_> = vec![1, 2, 3].iter().map(|x| x + 1).collect();
+    let data = vec![1, 2, 3];
+    let mut foo = data.iter().map(|x| x + 1);
 
-    println!("{:?}", foo);
+    let mut new_vector = vec![];
+
+    while let Some(x) = foo.next() {
+        new_vector.push(x);
+    }
+
+    println!("{:?}", new_vector);
 }
